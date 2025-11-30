e8c216c3de7263aa2775e07d1be5d8a7a37d29e5
Billy Jaffray <billyjaf@outlook.com>
2025-12-01 10:15:11 +1030

    feat: fly port

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ mod bot;
 async fn main() {
     let app = routes::app_routes();
 
-    let address = "0.0.0.0:3600";
-    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    let port = std::env::var("PORT").unwrap_or(String::from("3600"));
+    let address = format!("0.0.0.0:{port}");
+
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
