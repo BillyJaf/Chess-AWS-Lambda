2277124d451f8410d4b7c61a7792deb749dd530b
Billy Jaffray <billyjaf@outlook.com>
2025-12-01 18:57:31 +1030

    refactor: renamed health file

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,12 @@
 use axum::Router;
 
-pub mod health;
+pub mod health_check;
 pub mod legal_moves;
 pub mod best_move;
 
 pub fn app_routes() -> Router {
     Router::new()
-        .nest("/health_check", health::routes())
+        .nest("/health_check", health_check::routes())
         .nest("/legal_moves", legal_moves::routes())
         .nest("/best_move", best_move::routes())
 }
