1c79f8e3fbef271b9c118addd914be6ad5566af8
Billy Jaffray <billyjaf@outlook.com>
2025-12-01 11:26:28 +1030

    fix: correct cors origin

--- src/routes/best_move.rs
+++ src/routes/best_move.rs
@@ -1,14 +1,14 @@
 use axum::{
     Router, http::{Method, header::CONTENT_TYPE}, routing::post
 };
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::{AllowOrigin, CorsLayer};
 
 use crate::handlers::best_move;
 
 pub fn routes() -> Router {
     // THIS ALLOWS ALL CONNECTIONS, ONLY USE THIS FOR DEV
     let cors = CorsLayer::new()
-        .allow_origin(Any)
+        .allow_origin(AllowOrigin::exact("https://frontend-chess-bot.netlify.app".parse().unwrap()))
         .allow_methods([Method::POST])
         .allow_headers([CONTENT_TYPE]);
 
--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,13 +1,13 @@
 use axum::{
     Router, http::{Method, header::CONTENT_TYPE}, routing::get
 };
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::{AllowOrigin, CorsLayer};
 
 use crate::handlers::health_check;
 
 pub fn routes() -> Router {
     let cors = CorsLayer::new()
-        .allow_origin(Any)
+        .allow_origin(AllowOrigin::exact("https://frontend-chess-bot.netlify.app".parse().unwrap()))
         .allow_methods([Method::GET])
         .allow_headers([CONTENT_TYPE]);
 
