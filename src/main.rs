0fca65f6254c8c9969b4ea512be9522e98da0a58
Billy Jaffray <billyjaf@outlook.com>
2025-12-01 10:45:18 +1030

    feat: fixed port and gitignore

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod bot;
 async fn main() {
     let app = routes::app_routes();
 
-    let port = std::env::var("PORT").unwrap_or(String::from("3600"));
+    let port = std::env::var("PORT").unwrap_or(String::from("8080"));
     let address = format!("0.0.0.0:{port}");
 
     let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
