7c464df62c00e9e69ce3b50817e5a43e6cf3b8ff
Billy Jaffray <billyjaf@outlook.com>
2025-12-01 11:09:40 +1030

    feat: fixed ps1 tests to point at backend

--- api-test.ps1
+++ api-test.ps1
@@ -1,6 +1,6 @@
 # Perform a health check:
 $Params = @{
-    Uri = 'http://localhost:3600/health_check'
+    Uri = 'https://backend-chess-bot.fly.dev/health_check'
     Method = 'Get'
 }
 
@@ -8,7 +8,7 @@ Invoke-RestMethod @Params | ConvertTo-Json
 
 # Send a FEN string (expect a 200 Success and legal moves bounceback):
 $Params = @{
-    Uri = 'http://localhost:3600/legal_moves'
+    Uri = 'https://backend-chess-bot.fly.dev/legal_moves'
     Method = 'Post'
     Body = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' | ConvertTo-Json
     ContentType = 'application/json'
@@ -18,7 +18,7 @@ Invoke-RestMethod @Params | ConvertTo-Json
 
 # Send an invalid FEN string (expect a 400 Bad Request):
 $Params = @{
-    Uri = 'http://localhost:3600/legal_moves'
+    Uri = 'https://backend-chess-bot.fly.dev/legal_moves'
     Method = 'Post'
     Body = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR p KQkq - 0 1' | ConvertTo-Json
     ContentType = 'application/json'
@@ -29,7 +29,7 @@ Invoke-RestMethod @Params | ConvertTo-Json
 # Send a valid FEN string that is checkmate (expect a 200 Success and legal moves bounceback):
 
 $Params = @{
-    Uri = 'http://localhost:3600/legal_moves'
+    Uri = 'https://backend-chess-bot.fly.dev/legal_moves'
     Method = 'Post'
     Body = 'rnbqkbnr/1pppp2p/p7/5ppQ/4P3/3P4/PPP2PPP/RNB1KBNR b KQkq - 0 1' | ConvertTo-Json
     ContentType = 'application/json'
