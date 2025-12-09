7bf8e4e7aa54580f3a84b15a2ccb0b723fa285fc
Billy Jaffray <billyjaf@outlook.com>
2025-12-09 20:10:44 +1030

    feat: fixed wrapping of backend

--- src/main.rs
+++ src/main.rs
@@ -24,28 +24,58 @@ async fn handler(req: Request) -> Result<Response<Body>, Error> {
         ("POST", "/best_move") => {
             let body = req.body();
             let result = handlers::best_move::best_move(body.clone()).await;
-            Ok(Response::builder()
-                .status(200)
-                .body(serde_json::to_string(&result)?.into())
-                .unwrap())
+            match result {
+                Ok(best_move_response) => {
+                    Ok(Response::builder()
+                        .status(200)
+                        .body(serde_json::to_string(&best_move_response)?.into())
+                        .unwrap())
+                }
+                Err(response_error) => {
+                    Ok(Response::builder()
+                        .status(400)
+                        .body(serde_json::to_string(&response_error.error)?.into())
+                        .unwrap())
+                }
+            }
         }
 
         ("POST", "/legal_moves") => {
             let body = req.body();
             let result = handlers::legal_moves::legal_moves(body.clone()).await;
-            Ok(Response::builder()
-                .status(200)
-                .body(serde_json::to_string(&result)?.into())
-                .unwrap())
+            match result {
+                Ok(legal_moves) => {
+                    Ok(Response::builder()
+                        .status(200)
+                        .body(serde_json::to_string(&legal_moves)?.into())
+                        .unwrap())
+                }
+                Err(response_error) => {
+                    Ok(Response::builder()
+                        .status(400)
+                        .body(serde_json::to_string(&response_error.error)?.into())
+                        .unwrap())
+                }
+            }
         }
 
         ("POST", "/validate_fen") => {
             let body = req.body();
             let result = handlers::validate_fen::validate_fen(body.clone()).await;
-            Ok(Response::builder()
-                .status(200)
-                .body(serde_json::to_string(&result)?.into())
-                .unwrap())
+            match result {
+                Ok(validate_fen_response) => {
+                    Ok(Response::builder()
+                        .status(200)
+                        .body(serde_json::to_string(&validate_fen_response)?.into())
+                        .unwrap())
+                    }
+                Err(response_error) => {
+                    Ok(Response::builder()
+                        .status(400)
+                        .body(serde_json::to_string(&response_error.error)?.into())
+                        .unwrap())
+                    }
+            }
         }
 
         _ => Ok(Response::builder()
